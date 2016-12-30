88cda4e94b0a37927502d18fd5c18174ac31d516
Anders <anders.wk.choi@gmail.com>
2016-12-30 22:31:22 +0900

    added base tab 2

---
+++ app/src/main/java/com/example/anders/cs496_proj2/GridViewAdapter.java
@@ -0,0 +1,56 @@
+package com.example.anders.cs496_proj2;
+
+import android.content.Context;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class GridViewAdapter extends BaseAdapter {
+    private Context context;
+
+    public Integer[] imageIds = {
+            R.drawable.dog1, R.drawable.dog2, R.drawable.dog3, R.drawable.dog4,
+            R.drawable.cat1, R.drawable.cat2, R.drawable.cat3, R.drawable.cat4,
+            R.drawable.lion1, R.drawable.lion2, R.drawable.lion3,
+            R.drawable.otter1, R.drawable.otter2, R.drawable.otter3,
+            R.drawable.polarbear1, R.drawable.polarbear2, R.drawable.polarbear3
+    };
+
+    public GridViewAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return imageIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return imageIds[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView image;
+        if (convertView == null) {
+            image = new ImageView(context);
+            final int height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 113, this.context.getResources().getDisplayMetrics());
+            final int width = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 113, this.context.getResources().getDisplayMetrics());
+            image.setLayoutParams(new GridView.LayoutParams(width, height));
+            image.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        } else {
+            image = (ImageView) convertView;
+        }
+        image.setImageResource(imageIds[position]);
+        return image;
+    }
+}
--- app/src/main/java/com/example/anders/cs496_proj2/Tab2Fragment.java
+++ app/src/main/java/com/example/anders/cs496_proj2/Tab2Fragment.java
@@ -1,20 +1,32 @@
 package com.example.anders.cs496_proj2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-/**
- * Created by q on 2016-12-30.
- */
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 public class Tab2Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.base_tab, container, false);
+        View view = inflater.inflate(R.layout.tab2fragment, container, false);
+
+        GridView gridView = (GridView) view.findViewById(R.id.gridView);
+        gridView.setAdapter(new GridViewAdapter(getActivity()));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(getActivity().getApplicationContext(), ImageSliderActivity.class);
+
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
 
         return view;
     }
