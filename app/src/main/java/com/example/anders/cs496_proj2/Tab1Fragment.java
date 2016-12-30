1527a38a18a43d24ebcd4a8a38c87d3214fd7605
Zeniuus <jsh56son@kaist.ac.kr>
2016-12-30 21:27:11 +0900

    Create 3 Fragments

---
+++ app/src/main/java/com/example/anders/cs496_proj2/Tab1Fragment.java
@@ -0,0 +1,21 @@
+package com.example.anders.cs496_proj2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by q on 2016-12-30.
+ */
+
+public class Tab1Fragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.base_tab, container, false);
+
+        return view;
+    }
+}
